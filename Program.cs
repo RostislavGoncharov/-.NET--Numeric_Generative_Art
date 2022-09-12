bbdfdcf9b0565da74c48db6ab19d7bc02dcce54c
RostislavGoncharov <87071941+RostislavGoncharov@users.noreply.github.com>
2022-09-12 22:17:07 +0300

    Add a method to add numbers (if any) to list

--- Program.cs
+++ Program.cs
@@ -4,23 +4,44 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string input = GetInput();
+        List<int> numbers = new List<int>();
 
-
+        while (numbers == null || !numbers.Any())
+        {
+            string input = GetInput();
+            numbers = AnalyzeInput(input);
+        }
+        Console.WriteLine("Success");
     }
 
     static string GetInput()
     {
-        string? _input = null;
+        string? input = null;
 
-        while (_input == null || _input == "")
+        while (input == null || input == "")
         {
             Console.Write("Enter any number: ");
-            _input = Console.ReadLine();
+            input = Console.ReadLine();
         }
 
-        return _input;
+        return input;
     }
 
-    
+    static List<int> AnalyzeInput(string input)
+    {
+        List<int> numbersToProcess = new List<int>();
+
+        foreach (char c in input)
+        {
+            try {
+                int number = Int32.Parse(c.ToString());
+                numbersToProcess.Add(number);
+            }
+            catch (FormatException) {
+                continue;
+            }
+        }
+
+        return numbersToProcess;
+    }
 }
